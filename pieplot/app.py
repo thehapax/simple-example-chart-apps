f907b6e4759aed3e133e2f6d6da5a3db55fb4215
divya chandran <divyachandran.ds@gmail.com>
2019-02-26 10:24:56 -0500

    add changed files

--- pieplot/app.py
+++ pieplot/app.py
@@ -10,43 +10,29 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
-# from plotly import tools
 import plotly.graph_objs as go
 
-# from dash.dependencies import Input, Output
-
-external_stylesheets = ['https://raw.githubusercontent.com/plotly/dash-app-stylesheets/master/dash-docs-base.css']
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_ebola.csv')
-df = df.fillna(method='ffill')
+df = df.dropna(axis=0)
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
     html.Div([
-        html.H1("Ebola Cases-2014"),
-        html.Div([dcc.Dropdown(
-            id='month-selected',
-            options=[{'label': f"Month: {i}", 'value': i} for i in df["Month"].unique()],
-            value=8,
-
-        )], style={
-            'margin': {
-                'right': 200,
-                'left': 200
-
-            },
-            'padding-right': 500,
-            'padding-left': 500,
-
-        })
-
-    ], style={
-        'textAlign': "center"
-    }),
+        html.H1("Ebola Cases-2014")], style={"textAlign": "center"}),
     dcc.Graph(id="my-graph"),
+    html.Div([dcc.Slider(
+        id='month-selected',
+        min=df["Month"].min(),
+        max=df["Month"].max(),
+        marks={i: 'Month {}'.format(i) for i in range(3, 12)},
+        value=8)
+    ], style={
+        'textAlign': "center", "margin": "30px", "padding": "10px"}),
 
-], className="container", style={"margin": "30px", "padding": "20px"})
+], className="container")
 
 
 @app.callback(
@@ -68,8 +54,7 @@ def update_graph(selected):
         )
         ],
         "layout": go.Layout(
-            title=f'Ebola cases reported in {selected}th month',
-            # width = 1000,
+            title=f'Cases reported monthly',
             margin={"l": 300, "r": 300, },
         )
 
