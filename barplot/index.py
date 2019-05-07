e8ff46e2512f1a80d85ba0db1d1b247f345db45c
divya chandran <divyachandran.ds@gmail.com>
2019-05-07 14:52:34 -0400

    change to multipage app

--- barplot/index.py
+++ barplot/index.py
@@ -1,23 +1,32 @@
+import os
+
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-from app1 import app
-from apps import new, code
+from app import app
+from apps import main, code
+
+if 'DYNO' in os.environ:
+    app_name = os.environ['DASH_APP_NAME']
+else:
+    app_name = 'dash-barplot/'
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div(id='page-content')
 ])
 
+
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
 def display_page(pathname):
-    print(pathname)
-    if pathname == '/apps/code':
-         return code.code_layout
+    if pathname is None or pathname.replace(app_name, '').strip('/') == '':
+        return main.layout
+    elif pathname.replace(app_name, '').strip('/') == 'code':
+        return code.layout
     else:
-        return new.app_layout
+        return main.layout
 
 
 if __name__ == '__main__':
